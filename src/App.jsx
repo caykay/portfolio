03315fcef5a4341a72b5f780b8383c76e661b8b0
caykay <kahemacassian@gmail.com>
2023-01-20 23:11:37 +1030

    Skeleton structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Portfolio</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
