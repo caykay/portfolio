1f6d14018c88dac655d9c1e337db416fb2d773b9
caykay <kahemacassian@gmail.com>
2023-01-20 21:49:04 +1030

    Github Workflow and Build Config - squashed
    update build.yaml
    - only create or force reset the build branch instead of checking out
    
    remame master to main branch
    
    Vite config
    - remove sourcemap: true build config
    
    Disable generate sourcemap on build
    - this commit did not work because sourcemap was configured on build within the vite.config.js file
    
    vite config
    - change the base directory for vite to point to "/" instead of repository name (only for github pages) so it can work with netlify deploys
    
    fix build.yaml error
    - syntax error fix
    
    update github workflow to create new build branch
    
    update branch name in build.yaml
    
    workflow build test

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/",
   server: {
     host: true,
     port: 3000,
   },
   build: {
-    sourcemap: true,
     outDir: "build",
   },
 });
